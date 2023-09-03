2edd838affe56fcca6288377d1a39c14a2a31586
Marat <almabekov.marat@gmail.com>
2023-09-03 15:39:30 +0600

    CountriesViewModel

---
+++ app/src/main/java/com/example/testgraphql/presentation/CountriesViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.testgraphql.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.testgraphql.domain.DetailedCountry
+import com.example.testgraphql.domain.GetCountriesUseCase
+import com.example.testgraphql.domain.GetCountryUseCase
+import com.example.testgraphql.domain.SimpleCountry
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CountriesViewModel(
+    private val getCountriesUseCase: GetCountriesUseCase,
+    private val getCountryUseCase: GetCountryUseCase,
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CountriesState())
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(isLoading = true)
+            }
+            _state.update {
+                it.copy(
+                    countries = getCountriesUseCase.execute(),
+                    isLoading = false
+                )
+            }
+        }
+    }
+
+    fun selectedCountry(code: String) {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    selectedCountry = getCountryUseCase.execute(code)
+                )
+            }
+        }
+    }
+
+    fun dismissCountryDialog() {
+        _state.update {
+            it.copy(
+                selectedCountry = null
+            )
+        }
+    }
+
+    data class CountriesState(
+        val countries: List<SimpleCountry> = emptyList(),
+        val isLoading: Boolean = false,
+        val selectedCountry: DetailedCountry? = null,
+    )
+}
